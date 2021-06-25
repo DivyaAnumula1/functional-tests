f2e5f18c3355ed003f6f0079015c8c7c1918aa73
DivyaAnumula1 <divya.anumula24@gmail.com>
2021-06-26 00:06:57 +0530

    cucumber tests added

--- src/test/java/com/example/restservice/stepDefinitions/Steps.java
+++ src/test/java/com/example/restservice/stepDefinitions/Steps.java
@@ -31,7 +31,7 @@ public class Steps {
     @Autowired
     RandomGeneratorService randomGeneratorService;
 
-    @InjectMocks
+    @Autowired
     MultiplcationService multiplcationService;
 
     @BeforeStep
